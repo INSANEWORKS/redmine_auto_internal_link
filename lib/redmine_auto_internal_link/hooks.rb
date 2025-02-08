d63e6a14d03b606ad7e4c177024f96a0ddc62b6f
Kota Shiratsuka <kota@insaneworks.co.jp>
2025-02-08 11:51:53 +0900

    feat: Refactor link conversion to apply for both Journal and Issue

--- lib/redmine_auto_internal_link/hooks.rb
+++ lib/redmine_auto_internal_link/hooks.rb
@@ -1,26 +1,28 @@
 module RedmineAutoInternalLink
   class Hooks < Redmine::Hook::ViewListener
-    # コメント保存時にフックする
-    def controller_issues_edit_after_save(context = {})
-      update_comment_internal_links(context[:journal])
-    end
+    # Apply link conversion to both Journal (comments) and Issue (ticket descriptions)
+    [Journal, Issue].each do |model|
+      model.class_eval do
+        after_save :convert_external_links
 
-    private
+        private
 
-    def update_comment_internal_links(journal)
-      return unless journal.notes.present?
+        def convert_external_links
+          target_field = self.is_a?(Journal) ? :notes : :description
+          return unless self[target_field].present?
 
-      host_with_path = Setting.host_name + Redmine::Utils.relative_url_root.to_s
-      external_link_pattern = %r{https?://#{Regexp.escape(host_with_path)}/issues/(\d+)(#note-\d+)?}
-      internal_link_format = '[#\1\2](#{path_to_issue(\1)}\2)'
+          host_with_path = Setting.host_name + Redmine::Utils.relative_url_root.to_s
+          external_link_pattern = %r{https?://#{Regexp.escape(host_with_path)}/issues/(\d+)(#note-\d+)?}
 
-      updated_notes = journal.notes.gsub(external_link_pattern) do
-        issue_id = Regexp.last_match(1)
-        note_id = Regexp.last_match(2)
-        "##{issue_id}#{note_id}"
+          updated_text = self[target_field].gsub(external_link_pattern) do
+            issue_id = Regexp.last_match(1)
+            note_id = Regexp.last_match(2)
+            "##{issue_id}#{note_id}"
+          end
+
+          update_column(target_field, updated_text) if updated_text != self[target_field]
+        end
       end
-
-      journal.update(notes: updated_notes) if updated_notes != journal.notes
     end
   end
 end
